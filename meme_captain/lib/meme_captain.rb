01a46b18e6a5b627727ad7815d74a181ffda3267
UVa student <student@student-VirtualBox.(none)>
2014-06-19 02:09:21 -0400

    Getting rid of meme idea

--- meme_captain/lib/meme_captain.rb
+++
@@ -1,8 +0,0 @@
-require 'meme_captain/caption'
-require 'meme_captain/caption_choice'
-require 'meme_captain/draw'
-require 'meme_captain/image_list'
-require 'meme_captain/meme'
-require 'meme_captain/memebg'
-require 'meme_captain/text_pos'
-require 'meme_captain/version'
--- meme_captain/lib/meme_captain/version.rb
+++
@@ -1,3 +0,0 @@
-module MemeCaptain
-  VERSION = '0.3.0'
-end
